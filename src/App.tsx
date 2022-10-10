dad3333785d17c5eb057839f9133666c8ff36d2d
ftr0 <warecommon@protonmail.com>
2022-10-10 16:14:26 +0200

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+
+import img1 from './w1.jpg';
+import img2 from './w2.jpg';
+import img3 from './w3.jpg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={img1} className="App-logo" alt="logo" />
+        <img src={img2} className="App-logo" alt="logo" />
+        <img src={img3} className="App-logo" alt="logo" />
       </header>
     </div>
   );
